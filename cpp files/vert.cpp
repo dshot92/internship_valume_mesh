d7bdb0e4e633b4f650be36fdf3410a7b5d80d9ca
DShot92 <dshot92@gmail.com>
2020-05-18 19:17:40 +0200

    Files

---
+++ cpp files/vert.cpp
@@ -0,0 +1,65 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+template<class M, class V, class E, class P>
+CINO_INLINE
+bool AbstractPolygonMesh<M,V,E,P>::vert_is_manifold(const uint vid, Color c) const
+{
+    for(uint eid : this->adj_v2e(vid))
+    {
+        if(!this->edge_is_manifold(eid)) return false;
+    }
+
+    std::vector<uint> p_star = this->vert_ordered_polys_star(vid);
+    std::unordered_set<uint> edge_set(p_star.begin(), p_star.end());
+
+    std::queue<uint> q;
+    q.push(p_star.front());
+
+    std::unordered_set<uint> visited;
+    visited.insert(p_star.front());
+
+    Color curr_color = this->poly_data(q.front()).color;
+
+    bool change1 = false;
+    bool change2 = false;
+    bool change3 = false;
+    bool found_color = false;
+
+    for(auto pid : p_star){
+        if(this->poly_data(pid).color == c){
+            found_color = true;     // Cycle over p_star untill first poly of selected cluster is found
+        }
+        if(found_color){
+            if(change2){
+                if(this->poly_data(pid).color != c){
+                    change3 = true;
+                }
+            }
+            if(change1){
+                if(this->poly_data(pid).color == c){
+                    //curr_color = this->poly_data(pid).color;
+                    change2 = true;
+                }
+            }
+            if(this->poly_data(pid).color != c){
+                //curr_color = this->poly_data(pid).color;
+                change1 = true;
+            }
+        }
+    }
+    return (!change3);
+}
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+template<class M, class V, class E, class P>
+CINO_INLINE
+std::vector<uint> AbstractPolygonMesh<M,V,E,P>::vert_boundary_edges(const uint vid) const
+{
+    std::vector<uint> b_edges;
+    for(uint eid : this->adj_v2e(vid)) if (edge_is_boundary(eid)) b_edges.push_back(eid);
+    return b_edges;
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
